f17e7c75b5c8b89ef64aaa1953c67d9af48fba94
christopher lopez <christopherwlopez89@gmail.com>
2022-10-05 21:07:19 -0700

    created form

---
+++ src/CityForm.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import Form from 'react-bootstrap/Form';
+
+class CityForm extends React.Component {
+    render() {
+        return (
+            <Container>
+
+    
+    
+    
+    
+    
+    
+    
+            </Container>
+        )
+    }
+}
+
+export default CityForm;
