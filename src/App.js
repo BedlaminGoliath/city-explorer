23357baea851cbf66f0aa5d2c25ddb2ba354b39a
christopher lopez <christopherwlopez89@gmail.com>
2022-10-10 21:23:43 -0700

    changes with Gaz

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from 'react'
 import './App.css';
 import axios from 'axios';
 import Map from './Map';
-import { Form, Button, } from 'react-bootstrap';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Accordion from 'react-bootstrap/Accordion';
+import Alert from 'react-bootstrap/Alert';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends React.Component {
   constructor(props) {
@@ -12,58 +16,81 @@ class App extends React.Component {
       location: '',
       lat: '',
       lon: '',
-      mapUrl: ''
+      mapUrl: '',
+      errorMessage: ''
     }
   }
   handleChange = (event) => {
     this.setState({ searchQuery: event.target.value })
   }
 
-  getLocation = async () => {
-
+  getLocation = async (event) => {
+    this.setState({searchQuery: event.target.value})
     // things to change for the sake of the pull request
     // key: YOUR_ACCESS_TOKEN
     // q: SEARCH_STRING
     // format: 'json'
+    try {
+      const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.searchQuery}&format=json`;
+      console.log('url ', url);
+      // .query
+      const response = await axios.get(url);
+      console.log('response Object: ', response);
+      console.log(response.data[0].lat, response.data[0].lon);
+      this.setState({ location: response.data[0].display_name, lat: response.data[0].lat, lon: response.data[0].lon, errorMessage: false });
 
-    const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.searchQuery}&format=json`;
-    console.log('url ', url);
-    // .query
-    const response = await axios.get(url);
-    console.log('response Object: ', response);
-    console.log(response.data[0].lat, response.data[0].lon);
-    this.setState({ location: response.data[0].display_name, lat: response.data[0].lat, lon: response.data[0].lon })
+    } catch (error) {
+      this.setState({ errorMessage: true })
+    }
   }
 
   render() {
     return (
       <>
-      <div className="App">
-        <div className='body'>
+        <div className="App">
+          <div className='body'>
 
-          <h1>City Explorer</h1>
+            <h1>City Explorer</h1>
 
-            <Form>
-          <div className='form'>
-            <Form.Label></Form.Label>
-            <Form.Control
-          type="text" onChange={this.handleChange}
-            placeholder="search for your city here"
-             />
-          <Button onClick={this.getLocation}>Explore!</Button>
-            </div>
+            <Form >
+              <Form.Control
+                type="text" onChange={this.handleChange}
+                placeholder="search for your city here"
+              />
+              <Button onClick ={this.getLocation}>Explore!</Button>
+              {this.state.errorMessage &&
+                < Alert key='primary' type='danger'><p> enter a valid query</p></Alert>}
             </Form>
-          <h2>The City you searched for:{this.state.location.display_name}</h2>
-          {this.state.location &&
-          <Map map={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&center=${this.state.lat},${this.state.lon}&zoom=12`} />
-        }
-          <p>{this.state.lat}, {this.state.lon}</p>
-            
-        </div>
-        {/* <Container>
+            <h2>The City you searched for: {this.state.location}</h2>
+            {this.state.location &&
+              <Map map={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&center=${this.state.lat},${this.state.lon}&zoom=12`} />
+            }
+            <Accordion>
+              <Accordion.Item eventKey='0'>
+                <Accordion.Header>
+                  City Name:
+                </Accordion.Header>
+                <Accordion.Body>
+                  <h2>{this.state.location}</h2>
+                </Accordion.Body>
+              </Accordion.Item>
+              <Accordion.Item eventKey='1'>
+                <Accordion.Header>
+                  lat and lon:
+                </Accordion.Header>
+                <Accordion.Body>
+                  <h2> lat {this.state.lat}, lon {this.state.lon}</h2>
+                </Accordion.Body>
+              </Accordion.Item>
 
-      </Container> */}
-      </div>
+
+
+            </Accordion>
+          </div>
+
+
+
+        </div>
       </>
     );
   }
